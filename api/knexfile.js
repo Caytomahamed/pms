10a9543bd62b32ba886e80d0ef07b136c2667afa
cayto <aidarous1143@gmail.com>
2025-01-15 23:13:59 +0300

    🏥 hosting the database using render

--- api/knexfile.js
+++ api/knexfile.js
@@ -8,14 +8,8 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    client: 'mysql',
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
     pool: { min: 0, max: 20 },
     migrations: {
       directory: './database/migrations',
